68ff3ab555d016ff4358bf9c02de73b61b87a563
chai <cmilethz@gmail.com>
2026-05-21 10:34:06 -0500

    Fix rutas finales

--- apps/bioearth-template-style/vite.config.js
+++ apps/bioearth-template-style/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bioearth-prototipos/apps/bioearth-seoly-motion/dist/'
+  base: '/bioearth-prototipos/apps/bioearth-template-style/dist/'
 })
